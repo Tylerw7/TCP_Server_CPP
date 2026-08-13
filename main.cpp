d8ea211ad3bd554c90f9fe16f93a8420271ecb2f
Tylerw7 <t4wettach@gmail.com>
2026-08-12 21:28:37 -0700

    address and port binded

--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 
 int main() {
 
-    int server_id = socket(AF_INET, SOCK_STREAM, 0);
+    // Whats Happening?
+    // - AF_INET: tells the OS "I want to use IPv4" EX: 127.0.0.1
+    // - SOCK_STREAM: tells OS "I want a stream-oriented socket"
+    // TCP gives us a reliable, ordered byte stream. TCP is responsible for making sure the other side receives those bytes reliably and in order. 
+    // 0: "Use the default protocol for this socket type."
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    std::cout << "Socket: " << server_id << "\n";
-    
+    // Address structure
+    sockaddr_in server_address{};
+
+    // Set the address family
+    server_address.sin_family = AF_INET;
+
+    // Set the IP Address
+    // - htons() means: Host-To-Network-Short
+    server_address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+    // Set the Port
+    server_address.sin_port = 8080;
+
+    int result = bind(
+        server_fd,
+        (sockaddr*)&server_address,
+        sizeof(server_address)
+    );
+
+    std::cout << "Socket: " << result << "\n";
+
     return 0;
 }
