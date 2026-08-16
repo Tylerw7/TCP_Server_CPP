5b2549d185aec1951d783db343fd747f69d67c6d
Tylerw7 <t4wettach@gmail.com>
2026-08-15 21:39:01 -0700

    started work on helper functions

--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <cstring>
 #include <string>
 #include <sstream>
+#include <cstring>
+#include <unistd.h>
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+// Declared functions
+bool send_all(int socket_fd, const char* data, size_t length)
 
 int main() {
 
     // Whats Happening?
     // - AF_INET: tells the OS "I want to use IPv4" EX: 127.0.0.1
     // - SOCK_STREAM: tells OS "I want a stream-oriented socket"
-    // TCP gives us a reliable, ordered byte stream. TCP is responsible for making sure the other side receives those bytes reliably and in order. 
-    // 0: "Use the default protocol for this socket type."
+    // - TCP gives us a reliable, ordered byte stream.
+    // - 0: "Use the default protocol for this socket type."
+
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (server_fd == -1) {
+        std::cerr << "Failed to create socket\n";
+        return 1;
+    }
 
     // Address structure
     sockaddr_in server_address{};
@@ -22,77 +33,125 @@ int main() {
     server_address.sin_family = AF_INET;
 
     // Set the IP Address
-    // - htons() means: Host-To-Network-Short
     server_address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
     // Set the Port
     server_address.sin_port = htons(8080);
 
+    // Bind socket to IP + port
     int result = bind(
         server_fd,
         (sockaddr*)&server_address,
         sizeof(server_address)
     );
 
-    std::cout << "Bind result: " << result << "\n";
-
-    result = listen(server_fd,10);
-
-    std::cout << "Listen result: " << result << "\n";
-
-    int client_fd = accept(server_fd, nullptr, nullptr);
-
-    std::cout << "Client connected!\n";
-    std::cout << "Client socket: " << client_fd << "\n";
-
-    char buffer [4096]; // array of 4096 characters (bytes) in memory
-    int bytes_recieved = recv(
-        client_fd,
-        buffer,
-        sizeof(buffer),
-        0
-    );
-
-    //Parse HTTP request
-
-    std::string request(buffer, bytes_recieved); // To get the length
-    size_t end_of_line = request.find("\r\n");
-    std::string request_line = request.substr(0, end_of_line);
-    std::istringstream stream(request_line);
-
-    std::string method;
-    std::string path;
-    std::string version;
-
-    stream >> method >> path >> version;
-
-    std::cout << "Method: " << method << '\n';
-    std::cout << "Path: " << path << '\n';
-    std::cout << "Version: " << version << '\n';
-
-    if (method == "GET" && path == "/") {
-        const char* response = 
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: text/plain\r\n"
-            "Content-Length: 12\r\n"
-            "\r\n"
-            "New Message 1";
-
-        send(client_fd, response, strlen(response),0);    
+    if (result == -1) {
+        std::cerr << "Failed to bind socket\n";
+        return 1;
     }
 
-    const char* response =
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/plain\r\n"
-        "Content-Length: 12\r\n"
-        "\r\n"
-        "Hello world!";
+    // Start listening
+    result = listen(server_fd, 10);
 
-    send(client_fd, response, strlen(response),0);    
+    if (result == -1) {
+        std::cerr << "Failed to listen on socket\n";
+        return 1;
+    }
 
+    std::cout << "Server listening on http://localhost:8080\n";
 
-    std::cout << "Bytes recieved: " << bytes_recieved << "\n";
-    std::cout.write(buffer, bytes_recieved);
+    // Keep accepting clients
+    while (true) {
+
+        int client_fd = accept(
+            server_fd,
+            nullptr,
+            nullptr
+        );
+
+        if (client_fd == -1) {
+            std::cerr << "Failed to accept client\n";
+            continue;
+        }
+
+        std::cout << "Client connected!\n";
+        std::cout << "Client socket: " << client_fd << "\n";
+
+        // Buffer for receiving TCP data
+        char buffer[4096];
+
+        // Store the complete HTTP request
+        std::string request;
+
+        // Keep receiving until we've received
+        // the end of the HTTP headers.
+        while (request.find("\r\n\r\n") == std::string::npos) {
+
+            int bytes_received = recv(
+                client_fd,
+                buffer,
+                sizeof(buffer),
+                0
+            );
+
+            if (bytes_received <= 0) {
+                break;
+            }
+
+            request.append(buffer, bytes_received);
+        }
+
+        // Parse HTTP request
+
+        size_t end_of_line = request.find("\r\n");
+
+        if (end_of_line != std::string::npos) {
+
+            std::string request_line =
+                request.substr(0, end_of_line);
+
+            std::istringstream stream(request_line);
+
+            std::string method;
+            std::string path;
+            std::string version;
+
+            stream >> method >> path >> version;
+
+            std::cout << "Method: " << method << '\n';
+            std::cout << "Path: " << path << '\n';
+            std::cout << "Version: " << version << '\n';
+
+            // Temporary HTTP response
+            const char* response =
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/plain\r\n"
+                "Content-Length: 21\r\n"
+                "\r\n"
+                "Server is running hi!";
+
+            send(
+                client_fd,
+                response,
+                strlen(response),
+                0
+            );
+        }
+
+        close(client_fd);
+
+        std::cout << "Client disconnected\n";
+    }
+
+    close(server_fd);
 
     return 0;
 }
+
+
+// ------------------------------------------------------------------------------
+// END OF MAIN FUNCTION
+// START OF HELPER FUNCTIONS
+// ------------------------------------------------------------------------------
+
+
